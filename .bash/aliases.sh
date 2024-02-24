e92151c2e3ba667174105e1361ae597809761273
ELIKESBIKES <ecloaiza@users.noreply.github.com>
2024-02-23 22:52:10 -0800

    a

--- .bash/aliases.sh
+++ .bash/aliases.sh
@@ -1,7 +1,7 @@
 alias vim="vi"
 alias rm="rm -i"
 alias U="cd .."
-alias ls="ls -lA --color=yes | less -r -E -X"
+alias ls="ls -l --color=yes | less -r -E -X"
 alias cp="cp -ip"
 alias mv="mv -i"
 alias ll="ls -A | less -r -E -X"

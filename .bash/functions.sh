fc5281cddc8a259e3f8241bb311929d16949e4d2
Emmanuel Loaiza <13919542+elikesbikes@users.noreply.github.com>
2025-10-19 13:52:44 -0700

    put

--- .bash/functions.sh
+++ .bash/functions.sh
@@ -2,3 +2,11 @@
 function colormap() {
   for i in {0..255}; do print -Pn "%K{$i}  %k%F{$i}${(l:3::0:)i}%f " ${${(M)$((i%6)):#3}:+$'\n'}; done
 }
+
+function dockerent() {
+  sudo docker exec -it $1 /bin/bash
+}
+
+function dockerents() {
+  sudo docker exec -it $1 /bin/sh
+}
